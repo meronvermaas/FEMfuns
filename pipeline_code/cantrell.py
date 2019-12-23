447c8dd3cc662294defaad06c65245f7232fd1ce
meronvermaas <37903632+meronvermaas@users.noreply.github.com>
2019-12-23 15:39:17 +0100

    Update cantrell.py

--- pipeline_code/cantrell.py
+++ pipeline_code/cantrell.py
@@ -4,17 +4,7 @@ import numpy as np
 def double_layer_impedance(frequency):
     #assuming doublelayer = 1e-9 #1nm
 
-    if frequency == 0:
-        frequency = 1e-16
-        #return (338.,0.) #freq independent values from joucla
-        #error('double layer impedance at frequency of zero can not be calculated, divide by 0')
-
-
-    K = 1.57 #Ohm m2 s-beta
-    beta = 0.91
-    omega = 2*np.pi*frequency
-
-    Zcpa = K*(1j*omega)**-beta # equation (3) in cantrell
+    frequency = np.where(frequency==0,1e-20,frequency)
 
     I0 = 6.41e-4 #Am-2
     T = 298 #K
@@ -24,6 +14,13 @@ def double_layer_impedance(frequency):
 
     Rct = R*T/(n*F*I0) # equation (5) in cantrell
 
-    impedivity = Zcpa*Rct/(Zcpa+Rct)
+    K = 1.57 #Ohm m2 s-beta
+    beta = 0.91
+    omega = 2*np.pi*frequency
+
+    Zcpa = K*(1j*omega)**-beta # equation (3) in cantrell
+
+    impedivity = (Zcpa*Rct)/(Zcpa+Rct)
     admittivity = 1./impedivity
-    return np.real(admittivity), np.imag(admittivity)
+
+    return admittivity
