3d2311bc275cf584f035923d1b2e1136105a946d
meronvermaas <37903632+meronvermaas@users.noreply.github.com>
2022-02-23 17:02:16 +0100

    Update point_in_surf.m
    - fix such that normal is not rotated afterwards
    - depth is taken from edge size
    - edge sizes need to be approximately the same size

--- external/fieldtrip/point_in_surf.m
+++ external/fieldtrip/point_in_surf.m
@@ -1,29 +1,27 @@
-function point = point_in_surf(surface, depth)
+function point = point_in_surf(surface)
 
-% POINT_IN_SURF returns a point that is directly below the surface.
-% the depth of it under the surface is default set to 1e-6 to make sure the
-% point is not in another surface.
+% POINT_IN_SURF returns a point that is directly below the surface,
+% by taking a face and using the normal the find this coordinate.
 
-if ~exist('depth', 'var') || isempty(depth)
-    depth = 1e-6;
+%select points of a triangle with fairly similarly sized edges
+for ii = 1:size(surface.tri,1)
+    p1 = surface.pos(surface.tri(ii,1),1:3);
+    p2 = surface.pos(surface.tri(ii,2),1:3);
+    p3 = surface.pos(surface.tri(ii,3),1:3);
+    if std([norm(p2-p1) norm(p3-p1) norm(p3-p2)]) < 1
+        break
+    end
 end
 
-% center of the surface
-surfcenter = mean(surface.pos,1);
+%TODO raise error if no triangle was selected with similar sized edges
 
-% calculate the normal of the first triangle
-p1 = surface.pos(surface.tri(1,1),:);
-p2 = surface.pos(surface.tri(1,2),:);
-p3 = surface.pos(surface.tri(1,3),:);
+% calculate the normal of the triangle
 N = cross(p2-p1, p3-p1,2);
 N = N ./ sqrt(sum(N.^2,2));
 
 %centroid of the triangle
 ctd = (p1+p2+p3)/3;
 
-%make sure the normal points outside wrt the center of the surface
-sdot = sign(dot(ctd-surfcenter,N,3));
-N = sdot .* N;
-
 %get a point just below the surface
-point = ctd - N*depth;
+depth = norm(p3-p1)*1e-6; % fraction of the edge size under the triangle surface
+point = ctd + N*depth;
